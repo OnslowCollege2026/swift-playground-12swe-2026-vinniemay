8b825188c22bacfc9364e8856af3461dc640b0c5
Vin ☆ <vinnie.may@student.onslow.school.nz>
2026-02-23 23:07:33 +0000

    Challenge Task WIP

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -1,50 +1,52 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-func readNumber(prompt: String) -> Double {
-    print([prompt])
-    var returnValue = -1.0
-    guard let input = readLine(), let number = Double(input), number > 0 else {
-        print("Invalid number.")
-        return readNumber(prompt: prompt)
+
+func menuChoice() {
+    print("☆==== ⭒Egg Shop⭒ ====☆")
+    print("1. Add eggs")
+    print("2. Sell eggs")
+    print("3. Show current stock")
+    print("4. Show total eggs sold")
+    print("5. Exit")
+    print("Choose an option:")
     }
-    returnValue = number
-    return returnValue
-}
+
+func invalidInput() {
+    print("Invalid option. Please re-enter with a valid number.")
+    }
+
+
 
 @main
 struct SwiftPlayground {
     static func main() {
-        let maximumItemVolume = 2.0
+        var menuNeeded = "true" 
 
-        let roomLength = readNumber(prompt: "Enter the room length:")
-        let roomWidth = readNumber(prompt: "Enter the room width:")
-        let roomHeight = readNumber(prompt: "Enter the room height:")
+        var eggsInStock = 0
+        var eggsSold = 0
 
-let roomArea = roomWidth*roomLength
-print("Room area: \(roomArea)m²")
-// calculates the are of the room and prints it out
-
-let roomVolume = roomArea*roomHeight
-print("Room volume: \(roomVolume)m³")
-// calculates the volume of the room and prints it out
-
-print("Furniture:")
-
-let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
-// stores volumes of funiture in constant 
-var totalFurnitureVolume = 0.0
-furnitureVolumes.enumerated().forEach { index, volume in
-print("Item \(index + 1): \(volume)m³") 
-// prints out the volume of each item of furniture
-if volume > maximumItemVolume {
-    print("Oversized item detected.")
-    //checks if the volume of an item is larger than the max, and if it is, prints a warning
-}
-
-totalFurnitureVolume = totalFurnitureVolume + volume
-}
-let usableVolume = roomVolume - totalFurnitureVolume
-print("Usable volume: \(usableVolume)m³")
-        
+        while menuNeeded == "true" {
+            menuChoice()
+            var userInput = readLine()!
+            var userChoice = Int(userInput)!
+                if userChoice == 1 {
+                    print("test1")
+                } else if userChoice == 2 {
+                    print("test2")
+                } else if userChoice == 3 {
+                    print("test3")
+                } else if userChoice == 4 {
+                    print("test4")
+                } else if userChoice == 5 {
+                    print("☆⭒Thank you for using Egg Shop.⭒☆")
+                    print("𝘊𝘭𝘰𝘴𝘪𝘯𝘨 𝘗𝘳𝘰𝘨𝘳𝘢𝘮...")
+                    menuNeeded = "false"
+                } else {
+                    invalidInput() 
+                    userInput = readLine()!
+                    userChoice = Int(userInput)!
+                }
+            
+        }
 }}
