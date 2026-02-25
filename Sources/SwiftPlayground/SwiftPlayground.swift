2a745e8665acd087f12deaf9bb42732269adab32
Vin ☆ <vinnie.may@student.onslow.school.nz>
2026-02-25 22:09:40 +0000

    egg shop wip

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -16,6 +16,14 @@ func invalidInput() {
     print("Invalid option. Please re-enter with a valid number.")
     }
 
+func addEggs(currentStock: Int, amount: Int) -> Int {
+    return currentStock + amount
+}
+
+func sellEggs(currentStock: Int, amount: Int) -> Int {
+    return currentStock - amount
+}
+
 
 
 @main
@@ -25,27 +33,65 @@ struct SwiftPlayground {
 
         var eggsInStock = 0
         var eggsSold = 0
+        var totalEggsSold = 0
+        var eggsAdded = 0
+        var totalEggsAdded = 0
+
 
         while menuNeeded == "true" {
             menuChoice()
             var userInput = readLine()!
-            var userChoice = Int(userInput)!
+            var userChoice = Int(userInput)
+            // calls function to print menu and asks user to select an option. stores input in variable
                 if userChoice == 1 {
-                    print("test1")
-                } else if userChoice == 2 {
-                    print("test2")
+                    
+                
+                    print("How many eggs would you like to add?")
+                        userInput = readLine()!
+                        eggsAdded = Int(userInput)!
+                            if eggsAdded >= 0, eggsAdded <= 100 {
+                            eggsInStock = addEggs(currentStock: eggsInStock, amount: eggsAdded) 
+                            totalEggsAdded = totalEggsAdded + eggsAdded
+                            
+                        } else if eggsAdded > 100 && eggsAdded < 0 {
+                            print("Too many eggs added at once. Please input a smaller number.")
+                            userInput = readLine()!
+                            eggsAdded = Int(userInput)!
+                        } else if eggsAdded < 0 {
+                            print("You cannot add a negative amount of eggs, theres a Sell eggs option for that. Please re-enter a valid number.")
+                            userInput = readLine()!
+                            eggsAdded = Int(userInput)!
+                        } else {
+                            invalidInput()
+                            userInput = readLine()!
+                            eggsAdded = Int(userInput)!}
+
+                        
+                    } else if userChoice == 2 {
+                    print("How many eggs would you like to sell?")
+                        userInput = readLine()!
+                        eggsSold = Int(userInput)!
+                            eggsInStock = sellEggs(currentStock: eggsInStock, amount: eggsSold)
+                            totalEggsSold = totalEggsSold + eggsSold
+                    // if user chooses to sell eggs, it will ask them to input an amount and will remove this from the amount of eggs in stock
+
                 } else if userChoice == 3 {
-                    print("test3")
+                    print("There are currently \(eggsInStock) eggs in stock.")
+                    // if user chooses to show current stock, it will print it out
+
                 } else if userChoice == 4 {
-                    print("test4")
+                    print("\(totalEggsSold) eggs have been sold.")
+                    // if user chooses to see how many eggs have been sold, it will print out the total
+
                 } else if userChoice == 5 {
                     print("☆⭒Thank you for using Egg Shop.⭒☆")
                     print("𝘊𝘭𝘰𝘴𝘪𝘯𝘨 𝘗𝘳𝘰𝘨𝘳𝘢𝘮...")
                     menuNeeded = "false"
+                    // if user chooses to exit the program, the while loop will end and the program will finish
+
                 } else {
                     invalidInput() 
-                    userInput = readLine()!
-                    userChoice = Int(userInput)!
+                    // if user inputs an invalid option,it will ask them to reinput and will go back to the start of the while loop to show the menu again
                 }
             
         }
