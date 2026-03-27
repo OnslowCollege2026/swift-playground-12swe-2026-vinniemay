52ffe9a656d8108ced024f73cf1a83c81bfbaafb
Vin ☆ <vinnie.may@student.onslow.school.nz>
2026-03-27 01:18:53 +0000

    Mar 27, 2026, 1:18 AM

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -15,36 +15,5 @@ func readNumber(prompt: String) -> Double {
 @main
 struct SwiftPlayground {
     static func main() {
-        let maximumItemVolume = 2.0
-
-        let roomLength = readNumber(prompt: "Enter the room length:")
-        let roomWidth = readNumber(prompt: "Enter the room width:")
-        let roomHeight = readNumber(prompt: "Enter the room height:")
-
-let roomArea = roomWidth*roomLength
-print("Room area: \(roomArea)m²")
-// calculates the are of the room and prints it out
-
-let roomVolume = roomArea*roomHeight
-print("Room volume: \(roomVolume)m³")
-// calculates the volume of the room and prints it out
-
-print("Furniture:")
-
-let furnitureVolumes = [1.2, 0.8, 2.5, 0.6, 1.0]
-// stores volumes of funiture in constant 
-var totalFurnitureVolume = 0.0
-furnitureVolumes.enumerated().forEach { index, volume in
-print("Item \(index + 1): \(volume)m³") 
-// prints out the volume of each item of furniture
-if volume > maximumItemVolume {
-    print("Oversized item detected.")
-    //checks if the volume of an item is larger than the max, and if it is, prints a warning
-}
-
-totalFurnitureVolume = totalFurnitureVolume + volume
-}
-let usableVolume = roomVolume - totalFurnitureVolume
-print("Usable volume: \(usableVolume)m³")
         
 }}
