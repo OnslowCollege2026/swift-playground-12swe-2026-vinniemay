e61e46b4ee5c09bca7818908692bc89191bfd5f5
Vin ☆ <vinnie.may@student.onslow.school.nz>
2026-04-28 01:14:07 +0000

    wip kumara

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -11,9 +11,10 @@ func homePage() {
     print("⋆｡°✩ Kumara Stall ✩°｡⋆")
     print("⤷ Enter number to open tab")
     print("1) Add stock")
-    print("2) Current stock")
-    print("3) Sale records")
-    print("4) Close program")
+    print("2) Sell stock")
+    print("3) Current stock")
+    print("4) Sale records")
+    print("5) Close program")
 
 }
 
@@ -21,23 +22,56 @@ func generalInvalidInput() {
     print("Invalid option. Please re-enter with a valid number.")
 }
 
-func addKumara(currentStock: Int, amount: Int) -> Int {
+func addKumara(currentStock: Double, amount: Double) -> Double {
     return currentStock + amount
 } 
+
+func sellKumara(currentStock: Double, amount: Double) -> Double  {
+    return currentStock - amount
+}
+    
+func printCurrentStock(currentStock: Double) {
+    print("There is currently \(currentStock)kgs of kumara in stock.")
+}
+
+func calculateCostNoBag(sold: Double) -> Double {
+    return sold * 3
+}
+
+func calculateBagCost(sold: Double) -> Double {
+    var bags = 0.0
+    var bagsCost = 0.0
+    
+    if sold <= 5 {
+        bagsCost = 0.2
+        return bagsCost
+    } else {
+    }
+        bags = sold / 5 
+        bagsCost = bags * 0.2
+        return bagsCost
+}
+
+func printSale(sale: Double, price: Double) {
+    print("You have sold \(sale)kgs of kumara for $\(price)")
+}
 
 @main
 struct SwiftPlayground {
     static func main() {
         let bags = 5000 
-        let kumaraKiloCost = 3
         let minimumKumara = 0.1
-        let maxStock = 50
+        let maxStock = 50.0
 
-        var kumaraSold = 0 // kilos
-        var kumaraAdded = 0 // kilos
-        var currentStock = 50 // kilos
+        var kumaraSold = 10.0 // kilos
+        var kumaraAdded = 0.0 // kilos
+        var currentStock = 50.0 // kilos
+        var money = 0.0
+
 
         var binCapacityLeft = maxStock - currentStock
+
+        var salePrice = 0.0
 
         var runProgram = "true"
 
@@ -52,37 +86,54 @@ struct SwiftPlayground {
         if tabToOpen == "1" {
             print("How much kumara would you like to add?")
             userInput = readLine()!
-            kumaraAdded = Int(userInput)
+            
 
                 if kumaraAdded >= 0 && kumaraAdded <= binCapacityLeft {
                     currentStock = addKumara(currentStock: currentStock, amount: kumaraAdded)
-                
+                    printCurrentStock(currentStock: currentStock)
+
                 } else if kumaraAdded > binCapacityLeft {
-                    print("There is not enough space in the bin. Please add a number lower than \(binCapacityLeft)")
+                    print("There is not enough space in the bin. Please add a number lower than \(binCapacityLeft)kgs.")
                     userInput = readLine()!
-                    kumaraAdded = Int(userInput)
+                    
+                    currentStock = addKumara(currentStock: currentStock, amount: kumaraAdded)
+                    printCurrentStock(currentStock: currentStock)
                 
                 } else if kumaraAdded < 0 {
                     print("You can not add a negative number. Please re-enter with a valid number.")
                     userInput = readLine()!
-                    kumaraAdded = Int(userInput)
+                    
+                    currentStock = addKumara(currentStock: currentStock, amount: kumaraAdded)
+                    printCurrentStock(currentStock: currentStock)
                 
                 } else {
                     generalInvalidInput()
                 }
 
 
-
-
-
-
         } else if tabToOpen == "2" {
-            print("test 2")
+            print("How much kumara would you like to sell?")
+            userInput = readLine()!
+                currentStock = sellKumara(currentStock: currentStock, amount: kumaraSold)
+                salePrice = calculateCostNoBag(sold: kumaraSold) + calculateBagCost(sold: kumaraSold)
+                money = money + salePrice
+                printSale(sale: kumaraSold, price: salePrice)
+                printCurrentStock(currentStock: currentStock)
+
+                
+
         } else if tabToOpen == "3" {
-            print("test 3")
+            printCurrentStock(currentStock: currentStock)
+
+
         } else if tabToOpen == "4" {
+            print("You have made $\(money) from the Kumara Stall")
+
+
+        } else if tabToOpen == "5" {
             print("Thank you for using Kumara Stall.")
             runProgram = "false"
+
         } else {
             generalInvalidInput()
         }
